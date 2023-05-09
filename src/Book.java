21ed4d0febd27786da12744fc98ce66681218dd1
Wladimir <weizen.w@gmx.de>
2023-05-09 21:36:40 +0200

    feat: add class Book

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,19 @@
 public class Book {
 
+  private final String autor;
+  private final String name;
+
+  public Book(String autor, String name) {
+    this.autor = autor;
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getAutor() {
+    return autor;
+  }
+
 }
