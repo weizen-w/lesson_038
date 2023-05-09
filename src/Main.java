bbb749933d21c8f94e9a826c658333c5fbb5b31b
Wladimir <weizen.w@gmx.de>
2023-05-09 22:02:02 +0200

    feat: add new code to class Main

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,19 @@ public class Main {
     Book warAndPeace = new Book("Лев Толстой", "Война и Мир");
     Book idiot = new Book("Фёдор Достоевский", "Идиот");
     Book newIdiot = new Book("Фёдор Достоевский", "Идиот");
+    String firstEquals = String.format("%s-\"%s\" и %s-\"%s\"", warAndPeace.getAutor(),
+        warAndPeace.getName(), idiot.getAutor(), idiot.getName());
+    if (warAndPeace.equals(idiot)) {
+      System.out.printf("Книги (" + firstEquals + ") одинаковые");
+    } else {
+      System.out.println("Книги (" + firstEquals + ") не одинаковые");
+    }
+    String secondEquals = String.format("%s-\"%s\" и %s-\"%s\"", newIdiot.getAutor(),
+        newIdiot.getName(), idiot.getAutor(), idiot.getName());
+    if (newIdiot.equals(idiot)) {
+      System.out.printf("Книги (" + secondEquals + ") одинаковые");
+    } else {
+      System.out.println("Книги (" + secondEquals + ") не одинаковые");
+    }
   }
 }
