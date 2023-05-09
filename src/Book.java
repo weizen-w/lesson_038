d0723c8ed68bafba23c161c26310b72a02697c56
Wladimir <weizen.w@gmx.de>
2023-05-09 21:51:28 +0200

    feat: add method Book.equals and Book.hashCode

--- src/Book.java
+++ src/Book.java
@@ -16,4 +16,19 @@ public class Book {
     return autor;
   }
 
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (!(obj instanceof Book book)) {
+      return false;
+    }
+    return autor.equals(book.getAutor()) && name.equals(book.getName());
+  }
+
+  @Override
+  public int hashCode() {
+    return autor.hashCode() + name.hashCode();
+  }
 }
